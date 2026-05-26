96a0f5198fc6a2c9d45701c413a221d249493ab2
NalydRs4 <ramirosilva_45@hotmail.com>
2026-05-25 20:58:25 -0500

    Update comments in PruebasUnitarias.java

--- poo_unidad_4/src/ups/poo/audiovisual/test/PruebasUnitarias.java
+++ poo_unidad_4/src/ups/poo/audiovisual/test/PruebasUnitarias.java
@@ -7,6 +7,7 @@ import ups.poo.audiovisual.modelos.Actor;
 import ups.poo.audiovisual.modelos.Documental;
 import ups.poo.audiovisual.modelos.Investigador;
 // Al clonar de GitHub, es posible que el IDE no reconozca los paquetes importados de pruebas. 
+// Ejecutar primero el programa y luego habilitar los test para evitar que sobreescriba los archivos.
 // En ese caso, selecionar source actions y Genetate o Enable Tests.
 // Luego, ejecutar las pruebas desde el menú de Run, seleccionando la clase PruebasUnitarias.
 import static org.junit.Assert.assertEquals;
